9a508ffb5ab9a3e521565592cf67311bf7888340
TWind <thanwinde@outlook.com>
2025-03-04 21:55:26 +0800

    优化了分布式锁，使其在集群中不会出现误删情况

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @Component
@@ -11,16 +12,24 @@ public class SimpleRedisLock {
     @Autowired
     private RedisTemplate redisTemplate;
 
+    static final String uuid = UUID.randomUUID().toString() + "-";
+
     public boolean tryLock(String name, Long expireTime) {
         long threadId = Thread.currentThread().getId();
-        String key = "lock:" + name;
-        Boolean b = redisTemplate.opsForValue().setIfAbsent(key, threadId, expireTime, TimeUnit.SECONDS);
+        //threadId区分线程，uuid区分JVM
+        String key = "lock:" + name ;
+        Boolean b = redisTemplate.opsForValue().setIfAbsent(key, uuid + threadId, expireTime, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(b);
     }
 
     public void unlock(String name) {
         String key = "lock:" + name;
-        redisTemplate.delete(key);
+        long threadId = Thread.currentThread().getId();
+        String id = (String) redisTemplate.opsForValue().get(key);
+        if (id == uuid + threadId) {
+            redisTemplate.delete(key);
+        }
+
     }
 
 }
